a8ee90fcc28875dc21ff6e014e74f83921a82402
rookie <yingkongshi11@gmail.com>
2020-10-27 19:24:03 +0800

    add stetho

--- app/src/main/java/com/engineer/android/mini/MinApp.kt
+++ app/src/main/java/com/engineer/android/mini/MinApp.kt
@@ -2,6 +2,7 @@ package com.engineer.android.mini
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.facebook.stetho.Stetho
 
 /**
  * Created on 2020/9/13.
@@ -16,6 +17,7 @@ class MinApp : Application() {
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+        Stetho.initializeWithDefaults(this)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 }
