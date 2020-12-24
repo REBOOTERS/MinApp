829abe5613c0bbabef672e02589a1c8978c7e8e0
rookie <yingkongshi11@gmail.com>
2020-12-24 10:46:28 +0800

    fix some issue

---
+++ next/build/host/generated/source/buildConfig/debug/com/engineer/flutter/next/host/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.engineer.flutter.next.host;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.engineer.flutter.next.host";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
