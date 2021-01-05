d03cafd4e0eb77005f89bfa41164375495a2accc
rookie <yingkongshi11@gmail.com>
2021-01-05 22:49:12 +0800

    test lrucache

--- app/src/main/java/com/engineer/android/mini/MinApp.kt
+++ app/src/main/java/com/engineer/android/mini/MinApp.kt
@@ -1,7 +1,9 @@
 package com.engineer.android.mini
 
 import android.app.Application
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.collection.LruCache
 import com.facebook.stetho.Stetho
 
 /**
@@ -21,5 +23,19 @@ class MinApp : Application() {
         INSTANCE = this
         Stetho.initializeWithDefaults(this)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        lruTest()
+    }
+
+    private fun lruTest() {
+        val lruCache = LruCache<Int, Char>(10)
+        for (i in 65..90) {
+            lruCache.put(i, i.toChar())
+        }
+        Log.e(MINI, "lruTest() called $lruCache")
+        val map = lruCache.snapshot()
+        map.keys.forEach {
+            Log.e(MINI,"key=$it,value=${lruCache[it]}")
+        }
     }
 }
