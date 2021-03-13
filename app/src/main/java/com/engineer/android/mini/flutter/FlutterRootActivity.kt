3dd371155e61b97e97dbcd0fd157c341d8ac6047
rookie <yingkongshi11@gmail.com>
2021-03-13 16:00:17 +0800

    add flutter module

--- app/src/main/java/com/engineer/android/mini/flutter/FlutterRootActivity.kt
+++ app/src/main/java/com/engineer/android/mini/flutter/FlutterRootActivity.kt
@@ -6,17 +6,31 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.engineer.android.mini.R
+import com.engineer.android.mini.databinding.ActivityFlutterRootBinding
+import io.flutter.embedding.android.FlutterActivity
 
 class FlutterRootActivity : AppCompatActivity() {
 
+
+    private lateinit var viewBinding: ActivityFlutterRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_flutter_root)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        viewBinding = ActivityFlutterRootBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        setSupportActionBar(viewBinding.toolbar)
+        viewBinding.toolbarLayout.title = title
+        viewBinding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+        viewBinding.contentScrolling.flutterActivity.setOnClickListener {
+            startActivity(
+                FlutterActivity
+                    .withNewEngine()
+                    .initialRoute("/my_route")
+                    .build(this)
+            );
         }
     }
 }
