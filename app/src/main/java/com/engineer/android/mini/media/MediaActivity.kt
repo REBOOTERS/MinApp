7b6ca34343b2d1176775fbc9f17a1459a72952c4
rookie <yingkongshi11@gmail.com>
2021-09-14 19:04:24 +0800

    a video demo

--- app/src/main/java/com/engineer/android/mini/media/MediaActivity.kt
+++ app/src/main/java/com/engineer/android/mini/media/MediaActivity.kt
@@ -1,5 +1,6 @@
 package com.engineer.android.mini.media
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -8,12 +9,16 @@ import android.widget.MediaController
 import com.engineer.android.mini.R
 import com.engineer.android.mini.databinding.ActivityMediaBinding
 import com.engineer.android.mini.ext.toast
+import com.engineer.android.mini.ui.BaseActivity
 import java.io.File
 
-class MediaActivity : AppCompatActivity() {
+class MediaActivity : BaseActivity() {
 
     private lateinit var viewBinding: ActivityMediaBinding
     val path = Environment.getExternalStorageDirectory().absolutePath + "/Movies/vv.mp4"
+
+    private var landscape = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMediaBinding.inflate(layoutInflater)
@@ -26,6 +31,17 @@ class MediaActivity : AppCompatActivity() {
             viewBinding.videoView.requestFocus()
         } else {
             "$path not exist".toast()
+        }
+
+        viewBinding.fullScreen.setOnClickListener {
+            if (landscape) {
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                viewBinding.fullScreen.setImageResource(R.drawable.ic_baseline_fullscreen_24)
+            } else {
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                viewBinding.fullScreen.setImageResource(R.drawable.ic_baseline_fullscreen_exit_24)
+            }
+            landscape = !landscape
         }
     }
 }
