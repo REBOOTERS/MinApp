2ab92f4f31d961a22bdae135cdf7d4c535f77810
rookie <yingkongshi11@gmail.com>
2022-01-14 13:09:53 +0800

    fix workflow issue
    audio test

--- app/src/main/java/com/engineer/android/mini/better/Demo.java
+++ app/src/main/java/com/engineer/android/mini/better/Demo.java
@@ -4,20 +4,38 @@ import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class Demo {
+    private static Disposable sDisposable;
+
     public static void main(String[] args) {
-        Observable.intervalRange(0L, 200L, 0, 1L, TimeUnit.SECONDS)
+
+        Observable.intervalRange(0L, 10L, 0, 1L, TimeUnit.SECONDS)
 //                .observeOn(Schedulers.computation())
                 .doOnNext(aLong -> {
+                    int h = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
                     int m = Calendar.getInstance().get(Calendar.MINUTE);
                     int s = Calendar.getInstance().get(Calendar.SECOND);
-                    System.out.printf("%d,%s:%s\n", aLong, m, s);
+                    System.out.printf("%d,%s:%s:%s\n", aLong, h, m, s);
+
+                    if (aLong == 5) {
+                        if (sDisposable != null) {
+                            sDisposable.dispose();
+                        }
+                    }
+                })
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        sDisposable = disposable;
+                    }
                 })
                 .doOnComplete(() -> System.out.println("complete")).subscribe();
 
         try {
-            Thread.sleep(200000);
+            Thread.sleep(11000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
