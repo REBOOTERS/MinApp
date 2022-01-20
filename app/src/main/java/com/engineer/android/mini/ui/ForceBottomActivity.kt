05d69e4eeaaf6556fcdf0c700b10441f2a0c7237
rookie <yingkongshi11@gmail.com>
2022-01-20 10:16:49 +0800

    some test

--- app/src/main/java/com/engineer/android/mini/ui/ForceBottomActivity.kt
+++ app/src/main/java/com/engineer/android/mini/ui/ForceBottomActivity.kt
@@ -2,10 +2,13 @@ package com.engineer.android.mini.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.engineer.android.mini.R
 
+private const val TAG = "ForceBottomActivity"
 class ForceBottomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +20,8 @@ class ForceBottomActivity : AppCompatActivity() {
             textView.text = i.toString()
             ll.addView(textView)
         }
+
+        val ok = !TextUtils.isEmpty(null)
+        Log.d(TAG, "onCreate() called with: $ok")
     }
 }
