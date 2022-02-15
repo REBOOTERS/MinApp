1ab0789ddf9da4f3d0db7a9079d1e5d1aff9faf1
Rookie <yingkongshi11@gmail.com>
2022-02-15 19:52:11 +0800

    add compose demo

--- compose/src/main/java/com/engineer/compose/ui/MainComposeActivity.kt
+++ compose/src/main/java/com/engineer/compose/ui/MainComposeActivity.kt
@@ -3,13 +3,24 @@ package com.engineer.compose.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.engineer.compose.R
 import com.engineer.compose.ui.ui.theme.MiniAppTheme
 
+/**
+ * https://developer.android.google.cn/jetpack/compose/tutorial
+ */
 class MainComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +28,38 @@ class MainComposeActivity : ComponentActivity() {
             MiniAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    messageCard(Message("lucy", "hello world"))
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun messageCard(msg: Message) {
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "null",
+            modifier = Modifier.size(40.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    MiniAppTheme {
-        Greeting("Android")
-    }
+fun pre() {
+    val msg = Message("mike", "where are you from")
+    messageCard(msg)
 }
