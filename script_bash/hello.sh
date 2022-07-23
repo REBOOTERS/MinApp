f42868d20cce1cde375d759b31dd5144145a2e03
rookie <yingkongshi11@gmail.com>
2022-07-23 20:48:53 +0800

    fix all

--- script_bash/hello.sh
+++ script_bash/hello.sh
@@ -3,12 +3,12 @@
 echo hello world
 
 get_special_line() {
-  line1=$(grep LifeCycle_PanelActivity "$1" | head -1 )
+  line1=$(grep LifeCycle_PanelActivity "$1" | head -3 )
   echo "$line1"
-  line2=$(grep LifeCycle_PanelActivity "$1" | head -1 | awk '{ print $1,$2}')
+  line2=$(grep LifeCycle_PanelActivity "$1" | head -3 | awk '{ print $1,$2}')
   echo "$line2"
-  line3=$(grep LifeCycle_PanelActivity "$1" | head -1 | awk '{ print $3,$4,$5,$6,$7}')
-  echo "$line3"
+#  line3=$(grep LifeCycle_PanelActivity "$1" | head -2 | awk '{ print $3,$4,$5,$6,$7,$8,$9,$10}')
+#  echo "$line3"
 }
 
 #echo "$every_line"
@@ -19,9 +19,11 @@ for_each_dir() {
     echo "$file"
     get_special_line "$file"
   done
-
-  # 17.1MB/1000000 TB/台 * 4100台 *1.137*108元/TB=8.6元
-
 }
 
 for_each_dir "logs/"
+
+for num in {1..10}
+do
+  echo "$num"
+done
