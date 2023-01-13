bd032ca9d15f384839bbcd8373643ed02f0e8fb2
rookie <yingkongshi11@gmail.com>
2023-01-13 17:08:59 +0800

    update lib

--- app/src/main/java/com/engineer/android/mini/jetpack/FragmentManagerActivity.kt
+++ app/src/main/java/com/engineer/android/mini/jetpack/FragmentManagerActivity.kt
@@ -2,8 +2,11 @@ package com.engineer.android.mini.jetpack
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.engineer.android.mini.R
 import com.engineer.android.mini.databinding.ActivityJetpackBinding
+import com.engineer.android.mini.ext.toast
 import com.engineer.android.mini.jetpack.java.FooFragment
 import com.engineer.android.mini.jetpack.kotlin.BarFragment
 import com.engineer.android.mini.ui.BaseActivity
@@ -24,10 +27,25 @@ class FragmentManagerActivity : BaseActivity() {
         fooFragment = FooFragment.newInstance("Foo", "Foo1")
         viewBinding.upFab.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.up_content, fooFragment)
+                .replace(R.id.up_content, fooFragment,"zyq_zyq")
                 .commitAllowingStateLoss()
             refreshStatus()
+
+
         }
+
+        viewBinding.testFab.setOnClickListener {
+            val fragment = supportFragmentManager.findFragmentByTag("zyq_zyq")
+            val view = fooFragment?.view?.findViewById<View>(R.id.text)
+            Log.i(TAG,"view is "+view?.javaClass?.name)
+            if (view != null) {
+                view.setOnClickListener {
+                    "哈哈哈，I fount it".toast()
+                }
+            }
+        }
+
+
 
         viewBinding.upFabMinus.setOnClickListener {
             supportFragmentManager.beginTransaction()
