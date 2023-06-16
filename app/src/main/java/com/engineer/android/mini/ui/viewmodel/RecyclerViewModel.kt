bc1a181aeef414176b60520743ff0bf0d45c7d3b
rookie <yingkongshi11@gmail.com>
2023-06-16 19:50:43 +0800

    some test

---
+++ app/src/main/java/com/engineer/android/mini/ui/viewmodel/RecyclerViewModel.kt
@@ -0,0 +1,35 @@
+package com.engineer.android.mini.ui.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class RecyclerViewModel(app: Application) : AndroidViewModel(app) {
+
+    val datas = MutableLiveData<List<String>>()
+
+    fun loadData() {
+        val list = ArrayList<String>()
+        for (i in 0..3) {
+            list.add(i.toString())
+        }
+        datas.postValue(list)
+    }
+
+    fun insert(index: Int, value: String) {
+        val list = datas.value?.toMutableList()
+        list?.add(index, value)
+        list?.let {
+            datas.postValue(it)
+        }
+    }
+
+    fun remove(index: Int) {
+        val list = datas.value?.toMutableList()
+        if(index < 0 || index >= (list?.size ?: 0)) return
+        list?.removeAt(index)
+        list?.let {
+            datas.postValue(it)
+        }
+    }
+}
--- docs/view.md
+++ docs/view.md
@@ -3,3 +3,10 @@
 [“终于懂了” 系列：Android屏幕刷新机制—VSync、Choreographer 全面理解！
 ](https://juejin.cn/post/6863756420380196877)
 
+
+
+
+
+- ![cat](https://source.unsplasn.com/1280x720/?cat)
+- ![cat](https://source.unsplasn.com/1280x720/?猫)
+- ![芯片战争](https://img2.doubanio.com/view/subject/l/public/s34500182.jpg)
