4b22b9282cccf6fa4f791691e497648d2598cc58
rookie <yingkongshi11@gmail.com>
2023-07-10 10:03:36 +0800

    corner view

--- app/src/main/java/com/engineer/android/mini/ui/pure/CornerViewActivity.kt
+++ app/src/main/java/com/engineer/android/mini/ui/pure/CornerViewActivity.kt
@@ -1,12 +1,41 @@
 package com.engineer.android.mini.ui.pure
 
+import android.graphics.BitmapFactory
+import android.graphics.Outline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewOutlineProvider
 import com.engineer.android.mini.R
+import com.engineer.android.mini.databinding.ActivityCornerViewBinding
+import com.engineer.android.mini.ext.dp
+import com.engineer.android.mini.ui.BaseActivity
+import com.engineer.android.mini.util.ImageUtils
 
-class CornerViewActivity : AppCompatActivity() {
+class CornerViewActivity : BaseActivity() {
+
+    private lateinit var realBinding: ActivityCornerViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_corner_view)
+        realBinding = ActivityCornerViewBinding.inflate(layoutInflater)
+        setContentView(realBinding.root)
+
+
+        realBinding.phone.clipToOutline = true
+        realBinding.slider.addOnChangeListener { _, value, fromUser ->
+            Log.e(TAG, "onCreate() called with: value = $value, fromUser = $fromUser")
+            realBinding.phone.outlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View?, outline: Outline?) {
+                    val radius = value * 25.dp.toFloat()
+                    outline?.setRoundRect(0, 0, view!!.width, view.height, radius)
+                }
+            }
+        }
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.wallpaper_landscape)
+        val cornerBitmap = ImageUtils.toRoundCorner(
+            bitmap, 20.dp, ImageUtils.CORNER_BOTTOM_LEFT or ImageUtils.CORNER_BOTTOM_RIGHT or ImageUtils.CORNER_TOP_LEFT
+        )
+        realBinding.phone2.setImageBitmap(cornerBitmap)
     }
 }
