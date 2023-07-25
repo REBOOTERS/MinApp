c6505b602c7e438c3d4be9817b41e3b22a5d7a78
Rookie <yingkongshi11@gmail.com>
2023-07-25 20:10:37 +0800

    add some thing

--- app/src/main/java/com/engineer/android/mini/ui/tabs/ui/fragments/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/engineer/android/mini/ui/tabs/ui/fragments/MyItemRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.engineer.android.mini.ui.tabs.ui.fragments
 
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,7 @@ class MyItemRecyclerViewAdapter(
     private val listViewModel: ListViewModel,
     private val values: List<PlaceholderItem>
 ) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
-
+    private val TAG = "MyItemRecyclerViewAdapt"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         return ViewHolder(FragmentItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -32,5 +33,15 @@ class MyItemRecyclerViewAdapter(
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(val binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        Log.d(TAG, "onAttachedToRecyclerView() called with: recyclerView = $recyclerView")
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        Log.d(TAG, "onDetachedFromRecyclerView() called with: recyclerView = $recyclerView")
+    }
 
 }
