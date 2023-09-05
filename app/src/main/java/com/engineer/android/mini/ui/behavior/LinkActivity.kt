a567842377717a475a3d0576d3040ae0aeb5e3e5
rookie <yingkongshi11@gmail.com>
2023-09-05 15:57:11 +0800

    intent-filter test

---
+++ app/src/main/java/com/engineer/android/mini/ui/behavior/LinkActivity.kt
@@ -0,0 +1,12 @@
+package com.engineer.android.mini.ui.behavior
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.engineer.android.mini.R
+
+class LinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_link)
+    }
+}
