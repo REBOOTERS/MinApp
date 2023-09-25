a613d8aae8eff02f8c4aba040165cef1cebe7cf5
Rookie <yingkongshi11@gmail.com>
2023-09-25 17:31:12 +0800

    test receiver

--- app/src/main/java/com/engineer/android/mini/ui/behavior/MiniReceiver.kt
+++ app/src/main/java/com/engineer/android/mini/ui/behavior/MiniReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.engineer.common.notification.NotificationHelper
 
 
 class MiniReceiver : BroadcastReceiver() {
@@ -14,5 +15,11 @@ class MiniReceiver : BroadcastReceiver() {
         Log.d(TAG, "onReceive: $intent")
         val name = intent.getStringExtra("name")
         Log.i(TAG, "name is $name")
+
+        // adb shell am broadcast -a com.android.mini.receiver -f 0x01000000 -e name notify
+        // 可以触发这个广播
+        if("notify" == name) {
+            NotificationHelper.showNotification(context)
+        }
     }
 }
