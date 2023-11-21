d18c831a204ab554b2ae1f81dfdf8f5189b1b779
rookie <yingkongshi11@gmail.com>
2023-11-21 16:28:55 +0800

    lib update

---
+++ app/src/main/java/com/engineer/android/mini/ui/pure/DrawPageActivity.kt
@@ -0,0 +1,12 @@
+package com.engineer.android.mini.ui.pure
+
+import android.os.Bundle
+import com.divyanshu.draw.widget.DrawView
+import com.engineer.android.mini.ui.BaseActivity
+
+class DrawPageActivity:BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val board = DrawView(this)
+    }
+}
