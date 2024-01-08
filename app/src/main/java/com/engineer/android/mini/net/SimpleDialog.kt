9209aafb4b780f89239cdd3e982c0e6566c4883e
rookie <yingkongshi11@gmail.com>
2024-01-08 22:03:41 +0800

    test dialog

--- app/src/main/java/com/engineer/android/mini/net/SimpleDialog.kt
+++ app/src/main/java/com/engineer/android/mini/net/SimpleDialog.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.engineer.android.mini.R
 import com.engineer.android.mini.ext.dp
@@ -15,12 +16,21 @@ class SimpleDialog : DialogFragment() {
         return inflater.inflate(R.layout.dialog_layout, container, false)
     }
 
+
     override fun onStart() {
         super.onStart()
         val window = dialog?.window
-        // 设置宽度为铺满
+        window?.apply {
+//            setLayout(screenWidth - 100.dp, screenHeight / 2)
+            setBackgroundDrawableResource(R.drawable.gradient_bg)
+        }
+        val attrs = window?.attributes
+        attrs?.width = screenWidth - 100.dp
+        attrs?.dimAmount = 0.4f
+        attrs?.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND and
+                WindowManager.LayoutParams.FLAG_BLUR_BEHIND
+        window?.attributes = attrs
 
-        window?.setLayout(screenWidth - 100.dp, screenHeight / 2)
 //        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 }
