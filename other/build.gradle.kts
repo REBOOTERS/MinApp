4e309e5cf64a578b876a4b909dc7679019a3a2b8
rookie <yingkongshi11@gmail.com>
2024-05-21 15:12:44 +0800

    other module to kts

---
+++ other/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.tools.ksp)
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.engineer.other"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(project.findProperty("MYAPP_RELEASE_STORE_FILE") as String)
+            storePassword = project.findProperty("MYAPP_RELEASE_STORE_PASSWORD") as String
+            keyAlias = project.findProperty("MYAPP_RELEASE_KEY_ALIAS") as String
+            keyPassword = project.findProperty("MYAPP_RELEASE_KEY_PASSWORD") as String
+        }
+
+//        debug {
+//            keyAlias ="other"
+//            keyPassword= "123456"
+//            storeFile=file("other_debug.jks")
+//            storePassword ="123456"
+//        }
+    }
+
+    buildTypes {
+        release {
+            //混淆
+            isMinifyEnabled = true
+            //所以尽可能的减少第三方的使用 也是可以降低混淆的难度
+            //Zipalign优化
+            isZipAlignEnabled = true
+            // 移除无用的resource文件
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.findByName("release")
+        }
+        debug {
+            isMinifyEnabled = false
+//            signingConfig = signingConfigs.release
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.engineer.other"
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation(project(":deps:common"))
+
+}
+
+apply(from = "../custom-gradle/test-dep.gradle") // unit test
+apply(from = "../custom-gradle/viewmodel-dep.gradle") // view-model-lifecycle 全家桶
