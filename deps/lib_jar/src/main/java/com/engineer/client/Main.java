58433aaea507fa78a0c79d8edfa903677d5852cb
Rookie <yingkongshi11@gmail.com>
2024-08-01 15:43:28 +0800

    test json format

--- deps/lib_jar/src/main/java/com/engineer/client/Main.java
+++ deps/lib_jar/src/main/java/com/engineer/client/Main.java
@@ -1,7 +1,29 @@
 package com.engineer.client;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello module");
+
+        testJson();
+    }
+
+    private static void testJson() {
+        Student student = new Student();
+        student.name = "mike";
+        student.address = "beijing";
+        student.age = 11;
+
+        String s = JSON.toJSONString(student, SerializerFeature.PrettyFormat);
+        System.out.println(s);
+
+    }
+
+    static class Student {
+        public String name;
+        public int age;
+        public String address;
     }
 }
