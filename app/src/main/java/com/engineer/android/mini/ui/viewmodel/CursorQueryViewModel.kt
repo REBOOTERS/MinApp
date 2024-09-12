d0ad2a488b8d784dfd9b8d23945d7025261bf9be
Rookie <yingkongshi11@gmail.com>
2024-09-12 14:10:49 +0800

    fix all

--- app/src/main/java/com/engineer/android/mini/ui/viewmodel/CursorQueryViewModel.kt
+++ app/src/main/java/com/engineer/android/mini/ui/viewmodel/CursorQueryViewModel.kt
@@ -22,52 +22,63 @@ class CursorQueryViewModel @Inject constructor(app: Application) : AndroidViewMo
     internal val videoResults = MutableLiveData<List<Uri>>()
 
     fun loadGifs() {
-        thread {
+        val sectionHolder = buildSelectionAndArgs(listOf("image/gif"))
+        loadImageInternal(sectionHolder.first, sectionHolder.second)
+    }
 
-            val section = MediaStore.Images.Media.MIME_TYPE + "=?" + " AND " + MediaStore.MediaColumns.SIZE + ">0"
-            val sectionArgs = arrayOf("image/gif")
-            val imageList = ArrayList<Uri>()
-            val cursor = getApplication<MinApp>().contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                section,
-                sectionArgs,
-                "${MediaStore.MediaColumns.DATE_ADDED} desc"
-            )
-            if (cursor != null) {
-                while (cursor.moveToNext()) {
-                    val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
-                    val uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                    imageList.add(uri)
-                }
-                cursor.close()
-            }
-            imageResults.postValue(imageList)
+    fun loadNoAnimImages() {
+        val sectionHolder =
+            buildSelectionAndArgs(listOf("image/png", "image/jpeg", "image/x-ms-bmp","image/webp"))
+        loadImageInternal(sectionHolder.first, sectionHolder.second)
+
+    }
+
+    private fun buildSelectionAndArgs(mimeTypes: List<String>): Pair<String, Array<String>> {
+        val selection: String
+        val selectionArgs: Array<String>
+
+        if (mimeTypes.isEmpty()) {
+            // 如果没有指定 MIME 类型，则返回一个始终为真的条件（例如大小大于0）
+            selection = MediaStore.MediaColumns.SIZE + " > 0"
+            selectionArgs = arrayOf()
+        } else {
+            // 构建 MIME 类型的条件
+            val placeholders = mimeTypes.joinToString(", ") { "?" }
+            selection =
+                MediaStore.Images.Media.MIME_TYPE + " IN ($placeholders)" + " AND " + MediaStore.MediaColumns.SIZE + " > 0"
+            selectionArgs = mimeTypes.toTypedArray()
         }
+
+        return Pair(selection, selectionArgs)
     }
 
     fun loadImages() {
+        loadNoAnimImages()
+//        loadImageInternal(null, null)
+    }
 
+    private fun loadImageInternal(selection: String?, selectionArgs: Array<String>?) {
         thread {
             val imageList = ArrayList<Uri>()
             val cursor = getApplication<MinApp>().contentResolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 null,
-                null,
-                null,
+                selection,
+                selectionArgs,
                 "${MediaStore.MediaColumns.DATE_ADDED} desc"
             )
             if (cursor != null) {
                 while (cursor.moveToNext()) {
-                    val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
-                    val uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                    val id =
+                        cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
+                    val uri =
+                        ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
                     imageList.add(uri)
                 }
                 cursor.close()
             }
             imageResults.postValue(imageList)
         }
-
     }
 
     fun loadVideos() {
@@ -82,8 +93,10 @@ class CursorQueryViewModel @Inject constructor(app: Application) : AndroidViewMo
             )
             if (cursor != null) {
                 while (cursor.moveToNext()) {
-                    val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
-                    val uri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
+                    val id =
+                        cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
+                    val uri =
+                        ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
                     imageList.add(uri)
                 }
                 cursor.close()
