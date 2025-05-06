25428911741337f13bd28aa36e08e8ab8e864581
Rookie <yingkongshi11@gmail.com>
2025-05-06 19:57:54 +0800

    add version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,7 +160,7 @@ androidComponents {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
