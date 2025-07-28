bb80db658913d2a85891acef963967cffce3b304
Rookie <yingkongshi11@gmail.com>
2025-07-28 21:09:39 +0800

    add fix

--- app/src/main/java/com/engineer/android/mini/better/Demo.java
+++ app/src/main/java/com/engineer/android/mini/better/Demo.java
@@ -3,11 +3,17 @@ package com.engineer.android.mini.better;
 import android.os.Handler;
 import android.util.Log;
 
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.engineer.android.mini.util.TimeUtil;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -27,6 +33,84 @@ public class Demo {
     private static final AtomicBoolean S_INIT = new AtomicBoolean();
 
     public static void main(String[] args) {
+        TimeZone timeZone = TimeZone.getDefault();
+        Calendar calendar = Calendar.getInstance(timeZone);
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        long time1 = calendar.getTimeInMillis();
+        System.out.println(time1);
+
+
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+
+        long time2 = calendar.getTimeInMillis();
+        System.out.println(time2);
+
+        long now = System.currentTimeMillis();
+        System.out.println(now);
+        System.out.println(now > time1);
+        System.out.println(now < time2);
+
+        long diff = time2 - time1;
+        System.out.println(diff);
+        System.out.println(diff/60f);
+        System.out.println(diff/3600f);
+
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println(dayOfWeek);
+
+        /**
+         * {"STORAGE":{"mode":"cloud_storage","type":""},
+         * "TARGET_FOLLOW":"1","CRUISE_TIME":[{"cruiseTime":"08:00","cycle":
+         * "0,1,2,3,4,5,6","status":"0","time":"0800","timestamp":
+         * "1753488000000"},{"cruiseTime":"12:00","cycle":"0,1,2,3,4,5,6",
+         * "status":"0","time":"1200","timestamp":"1753502400000"},
+         * {"cruiseTime":"16:00","cycle":"0,1,2,3,4,5,6","status":"0"
+         * ,"time":"1600","timestamp":"1753430400000"},{"cruiseTime":"15:10",
+         * "cycle":"","status":"1","time":"1510","timestamp":"1753427400000"}],
+         * "CRUISE_ANGLE":{"end":{"pan":"-73","tilt":"0"},"extent":"180",
+         * "mode":"collect_point","start":{"pan":"61","tilt":"0"}},
+         * "cuei":"200049040000396","switchStatus":"1"}
+         */
+
+        fileTime();
+    }
+
+
+
+    public static void fileTime() {
+        Path filePath = Paths.get("Demo.java");
+
+        try {
+            // 获取基本文件属性
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+
+            System.out.println("文件创建时间: " + attrs.creationTime());
+            System.out.println("最后修改时间: " + attrs.lastModifiedTime());
+            System.out.println("最后访问时间: " + attrs.lastAccessTime());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testAny() {
+        testJson();
+
+        double value = ((1.8 / 1000000 * 9000000 * 1.137 * 108));
+        System.out.println("value is " + value);
+
+        BigDecimal dd = new BigDecimal("11.0");
+    }
+
+
+    private static void testCalendar() {
         TimeZone china = TimeZone.getTimeZone("GMT+08:00");
         Calendar calendar = Calendar.getInstance(china);
         calendar.setTime(new Date());
@@ -41,13 +125,6 @@ public class Demo {
         System.out.println("atomicBoolean current2 = " + S_INIT.get());
         System.out.println("atomicBoolean ==2 " + S_INIT.compareAndSet(false, true));
         System.out.println("atomicBoolean current3 = " + S_INIT.get());
-
-        testJson();
-
-        double value = ((1.8 / 1000000 * 9000000 * 1.137 * 108));
-        System.out.println("value is " + value);
-
-        BigDecimal dd = new BigDecimal("11.0");
     }
 
 
@@ -82,11 +159,7 @@ public class Demo {
         result.put("time", System.currentTimeMillis());
 
 
-        String beauty = JSON.toJSONString(result,
-                SerializerFeature.PrettyFormat,
-                SerializerFeature.SortField,
-                SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteDateUseDateFormat);
+        String beauty = JSON.toJSONString(result, SerializerFeature.PrettyFormat, SerializerFeature.SortField, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteDateUseDateFormat);
         System.out.println(beauty);
     }
 
@@ -105,14 +178,12 @@ public class Demo {
                             sDisposable.dispose();
                         }
                     }
-                })
-                .doOnSubscribe(new Consumer<Disposable>() {
+                }).doOnSubscribe(new Consumer<Disposable>() {
                     @Override
                     public void accept(Disposable disposable) throws Exception {
                         sDisposable = disposable;
                     }
-                })
-                .doOnComplete(() -> System.out.println("complete")).subscribe();
+                }).doOnComplete(() -> System.out.println("complete")).subscribe();
 
         try {
             Thread.sleep(11000);
@@ -142,6 +213,7 @@ public class Demo {
     private static void test2() throws Exception {
         throw new Exception("null");
     }
+
     private Runnable task = () -> Log.d("tag", "run() called");
 
     private void max(int a, int b) {
@@ -150,7 +222,6 @@ public class Demo {
 //            task;
             Log.d("tag", "222");
         });
-
 
 
         new Handler().post(task);
